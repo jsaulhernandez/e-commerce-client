988f8408a920c7cf8e16880ee41a4c0c702a6f59
Sony <jsaulhernandez98@gmail.com>
2025-01-04 12:45:36 -0600

    tag: getting userId from clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-
+import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
 import "./globals.css";
+// components
+import Header from "@/components/header";
+// utils
 import { cn } from "@/lib/utils";
 
 const urbanist = Urbanist({
@@ -15,15 +19,26 @@ export const metadata: Metadata = {
   description: "Have to food & share love",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { userId } = await auth();
+
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={cn("bg-background antialiased", urbanist.variable)}>
+          <Image
+            src="/img/hero.svg"
+            alt=""
+            className="absolute -z-10 top-0 right-0 w-full md:w-[60%]"
+            width={100}
+            height={100}
+          />
+          <Header userId={userId} />
+
           {children}
         </body>
       </html>
