f7a3dc06ed5f1f3379632aacd54ef0e23dbfc50b
Sony <jsaulhernandez98@gmail.com>
2025-01-09 15:10:38 -0600

    tag: adding zoom to image

--- app/(routes)/_components/chef-image.tsx
+++ app/(routes)/_components/chef-image.tsx
@@ -10,7 +10,7 @@ const ChefImage = ({ pathImage, alt, className }: ChefImageProps) => {
   return (
     <Card
       className={cn(
-        "shadow-lg relative rounded-md border-none flex flex-col items-center justify-center h-96 md:h-[520px] bg-hero/30",
+        "shadow-lg relative rounded-md border-none flex flex-col items-center justify-center h-96 md:h-[520px] bg-hero/30 overflow-hidden",
         className
       )}
     >
@@ -18,7 +18,7 @@ const ChefImage = ({ pathImage, alt, className }: ChefImageProps) => {
         src={pathImage}
         alt={alt ?? "Chef Image"}
         fill
-        className="w-full h-full object-contain"
+        className="w-full h-full object-contain transition-transform duration-300 transform hover:scale-110 hover:translate-y-10"
       />
     </Card>
   );
