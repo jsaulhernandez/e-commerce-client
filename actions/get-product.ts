ed32afc4890d330a3a47c596c93acac0bde61b7a
Sony <jsaulhernandez98@gmail.com>
2025-01-10 17:18:40 -0600

    fix: return a single object

--- actions/get-product.ts
+++ actions/get-product.ts
@@ -3,9 +3,9 @@ import { IProduct } from "@/data/interfaces/product.interface";
 // constants
 import { productsURL } from "@/data/constants";
 
-const getProduct = async (id: string): Promise<IProduct[]> => {
+const getProduct = async (id: string): Promise<IProduct> => {
   const res = await fetch(`${productsURL}/${id}`);
-  return res.json() as Promise<IProduct[]>;
+  return res.json() as Promise<IProduct>;
 };
 
 export default getProduct;
