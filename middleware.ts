6c67e6f43e218a772eb5f685b37917513f8b5d2f
Sony <jsaulhernandez98@gmail.com>
2025-01-18 12:53:24 -0600

    tag: removing public paths

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,6 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
-  "/menu",
-  "/menu/:productId",
   "/contact",
   "/sign-in(.*)",
   "/sign-up(.*)",
